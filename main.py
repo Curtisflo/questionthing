84f0614368454dc6ae4369a268baa3d90f001a8b
Curtisflo <74998524+Curtisflo@users.noreply.github.com>
2020-11-24 21:21:58 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-questions: = add_questions()
-questions: = link_questions()
-update(questions)
-
 class question:
     def __init__(self, num, question, subordinate, links):
         self.question = question
@@ -16,9 +12,11 @@ class links:
 
 
 def add_questions():
+    question_list = question(1, "hi", -1, links(1, 2)) 
     #access database
     #determine what the links are
     #separate the itmes into an array
+    #question_list.links.append(new_link)
     return question_list
 
 def link_questions(question_list):
@@ -27,17 +25,23 @@ def link_questions(question_list):
     greatest_index = -1
     for x in question_list:
         for x in question_list.links:
-            if questions_list.links.total > greatest_links:
-                greatest_links = questions_list.links.total
-                greatest_index = questoions_list.links.index
-            if question_list[greatest_index].likes => question_list.likes:
+            if question_list.links.total > greatest_links:
+                greatest_links = question_list.links.total
+                greatest_index = question_list.links.index
+            if question_list[greatest_index].likes == question_list.likes:
                 question_list.subordinate = greatest_index
             else:
                 question_list.subordinate = -1
         greatest_index = -1
         greatest_links = 0
+    return question_list
 
 def update(question_list):
     #alter everything's subordinate variable
     for x in question_list:
+        pass
         #whatever the fuck is the sql thing = question_list.subordinate
+
+questions = add_questions()
+questions = link_questions(questions)
+update(questions)
