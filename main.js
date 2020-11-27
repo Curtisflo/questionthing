ac78aec93fddedfba56d8f190656124c1418fb95
Curtis Floras <curtisfloras@gmail.com>
2020-11-27 15:22:58 -0500

    biwrughwrg

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
 var table = new Tabulator("#example-table", {
     height:"311px",
     columns:[
-    {title:"Name", field:"name"},
-    {title:"Progress", field:"progress", sorter:"number"},
-    {title:"Gender", field:"gender"},
-    {title:"Rating", field:"rating"},
-    {title:"Favourite Color", field:"col"},
-    {title:"Date Of Birth", field:"dob", hozAlign:"center"},
+    {title:"Question", field:"question"},
+    {title:"Likes", field:"likes", sorter:"number"},
+    {title:"likes_links", field:"likes_links"},
+    {title:"subordinate", field:"subordinate"},
     ],
 });
 
 //able to get the table name created by the user and store it in a variable now
 const check = (e) => {
-    const form = new FormData(e.target);
-    const tableName = form.get("tableName");
-    console.log(tableName);
-    return false
+  const form = new FormData(e.target);
+  const tableName = form.get("tableName");
+  console.log(tableName);
+  return false
 };
